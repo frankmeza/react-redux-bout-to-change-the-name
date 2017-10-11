bbd4191998ae1ed5b5148d6012b515728ac95269
Frank Meza <frankmezapdx@gmail.com>
2017-10-11 16:37:07 -0700

    refactor: setName now comes from Parent via props passed to Child

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import * as actions from '../actions'
 
 class Child extends Component {
     // 11. the store is passed into this component's props
@@ -18,26 +19,18 @@ class Child extends Component {
         this.setState({ name: event.target.value })
     }
 
-    // 13. this is the action that will be emitted to the store to change app state
-    setName(name) {
-        return {
-            type: "SET_NAME",
-            name
-        }
-    }
-
-    // 14. the action within setName is hidden within this 
+    // 13. the action within setName is hidden within this 
     // function, which is called from within the component
     handleSubmit() {
-        this.props.store.dispatch(this.setName(this.state.name))
+        this.props.dispatch(actions.setName(this.state.name))
     }
 
     render() {
         return (
             <div>
-                <h3>My name is {this.state.name}</h3> {/* 15. this is constantly updated from handleChange  */}
+                <h3>My name is {this.state.name}</h3> {/* 14. this is constantly updated from handleChange  */}
                 <input type="text" onChange={this.handleChange} />
-                {/* 16. handled above */}
+                {/* 15. handled above */}
                 <button onClick={this.handleSubmit}>Repeat name</button>
             </div>
         )

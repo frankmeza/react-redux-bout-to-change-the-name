3b4dad7e5e596b1825621857c02e65bd63c04869
Frank Meza <frankmezapdx@gmail.com>
2017-10-11 15:23:19 -0700

    refactor: add store, pass this into Parent as prop, subscribe #render function

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import Parent from './components/Parent'
 import Child from './components/Child'
-// redux
+
+// import redux, and reducer
 import { createStore } from 'redux'
 import changeNameApp from "./reducers/name_reducer"
 
-ReactDOM.render(<Parent />, document.getElementById('root'))
-registerServiceWorker()
+// instantiate a store using the reducer
+const store = createStore(changeNameApp)
+
+// render Parent component to DOM, passing in store as a prop to Parent
+const render = () => {
+    ReactDOM.render(
+        <Parent store={store}/>, 
+        document.getElementById('root')
+    )
+}
+
+// call function so that initial state is rendered
+render()
+
+// subscribe #render to listen to the store,
+// so it runs every time the store is updated
+store.subscribe(render)

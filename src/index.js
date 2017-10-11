104c7251e3ed2cbaacf55a8f6d93b421e1f38e05
Frank Meza <frankmezapdx@gmail.com>
2017-10-11 15:17:09 -0700

    refactor index.js to include redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 import Parent from './components/Parent'
 import Child from './components/Child'
+// redux
+import { createStore } from 'redux'
+import changeNameApp from "./reducers/name_reducer"
 
-ReactDOM.render(<Parent />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Parent />, document.getElementById('root'))
+registerServiceWorker()
